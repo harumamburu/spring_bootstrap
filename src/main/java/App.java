a00bd45bea05c896aab3c72642969acfd9cd169a
harumumburu <nikolay.kovalev92@gmail.com>
2015-10-27 19:29:11 +0300

    added application (primitive) logic

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,16 +1,19 @@
-/**
- * Created by Nikolay_Kovalev on 10/22/2015.
- */
 public class App {
 
     private Client client;
     private EventLogger eventLogger;
 
     public static void main(String[] args) {
+        App app = new App();
 
+        app.client = new Client(1l, "John Doe");
+        app.eventLogger = new ConsoleEventLogger();
+
+        app.logEvent("Event for user 1");
     }
 
     private void logEvent(String event) {
-
+        String message = event.replaceAll(String.valueOf(client.getId()), client.getFullName());
+        eventLogger.logEvent(message);
     }
 }
