bc732e91b8041132fe596eb551cc0045221958c9
harumumburu <nikolay.kovalev92@gmail.com>
2015-10-27 19:23:50 +0300

    added client and logger to the app class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,4 +2,15 @@
  * Created by Nikolay_Kovalev on 10/22/2015.
  */
 public class App {
+
+    private Client client;
+    private EventLogger eventLogger;
+
+    public static void main(String[] args) {
+
+    }
+
+    private void logEvent(String event) {
+
+    }
 }
--- src/main/java/RenameLogger.java
+++
@@ -1,7 +0,0 @@
-/**
- * Created by lilie0-ASUS on 10/27/2015.
- */
-public interface RenameLogger {
-
-    void logEvent(String event);
-}
