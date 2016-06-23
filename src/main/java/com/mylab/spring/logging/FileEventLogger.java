78ac71b3a933969919455a1f0bbb98609af0ba93
harumamburu <nikolay.kovalev92@gmail.com>
2016-06-23 18:33:35 +0300

    Add file event logger
    and commons-io dependency for that

---
+++ src/main/java/com/mylab/spring/logging/FileEventLogger.java
@@ -0,0 +1,40 @@
+package com.mylab.spring.logging;
+
+import com.mylab.spring.event.Event;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileEventLogger implements EventLogger {
+
+    protected String filename;
+    protected File logFile;
+    protected String encoding;
+
+    final String newLine = System.getProperty("line.separator");
+
+    public FileEventLogger(String filename, String encoding) {
+        this.filename = filename;
+        this.encoding = encoding;
+    }
+
+    @Override
+    public void logEvent(Event event) {
+        try {
+            FileUtils.writeStringToFile(logFile, event.toString() + newLine, encoding, true);
+        } catch (IOException exc) {
+            throw new RuntimeException("Failed to write an event to the log", exc);
+        }
+    }
+
+    protected void init() throws IOException {
+        logFile = new File(filename);
+        if (!logFile.exists()) {
+            logFile.createNewFile();
+        }
+        if (!logFile.isFile() || !logFile.canWrite()){
+            throw new IOException("Can't write to file");
+        }
+    }
+}
