9abbe62e3935d404a66a45dfe7654a3ef5c4ae1c
harumamburu <nikolay.kovalev92@gmail.com>
2016-06-24 11:50:22 +0300

    add combined event logger to use all created loggers

---
+++ src/main/java/com/mylab/spring/logging/CombinedEventLogger.java
@@ -0,0 +1,19 @@
+package com.mylab.spring.logging;
+
+import com.mylab.spring.event.Event;
+
+import java.util.List;
+
+public class CombinedEventLogger implements EventLogger {
+
+    private List<EventLogger> loggers;
+
+    public CombinedEventLogger(List<EventLogger> loggers) {
+        this.loggers = loggers;
+    }
+
+    @Override
+    public void logEvent(Event event) {
+        loggers.forEach(logger->logger.logEvent(event));
+    }
+}
