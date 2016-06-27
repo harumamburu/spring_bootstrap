10c36f915006adceb73744a1b1be608dd9266071
Nick <nikolay.kovalev92@gmail.com>
2016-06-27 15:38:51 +0300

    add value injection to event type

--- src/main/java/com/mylab/spring/event/Event.java
+++ src/main/java/com/mylab/spring/event/Event.java
@@ -2,6 +2,7 @@ package com.mylab.spring.event;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -44,6 +45,7 @@ public class Event {
         return type;
     }
 
+    @Value("INFO")
     public void setType(EventType type) {
         this.type = type;
     }
