bb353350c5e0633ee4eaae65aeda02720efd2fd4
Nick <nikolay.kovalev92@gmail.com>
2016-06-27 17:01:33 +0300

    Rework App class to work with annotation-based configs

--- src/main/java/com/mylab/spring/App.java
+++ src/main/java/com/mylab/spring/App.java
@@ -1,30 +1,41 @@
 package com.mylab.spring;
 
 import com.mylab.spring.client.Client;
+import com.mylab.spring.config.DateTimeBeanConfig;
+import com.mylab.spring.config.LoggingBeansConfig;
+import com.mylab.spring.config.PropertiesPlaceholderBeanConfig;
 import com.mylab.spring.event.Event;
 import com.mylab.spring.event.EventType;
 import com.mylab.spring.logging.EventLogger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import java.util.Map;
 
 @Component("app")
 public class App {
 
+    @Autowired
     private Client client;
+    @Resource(name = "loggersMap")
     private Map<EventType, EventLogger> loggers;
 
-    @Autowired
+    private App() {}
+
     private App(Client client, Map<EventType, EventLogger> loggers) {
         this.client = client;
         this.loggers = loggers;
     }
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext appContext = new ClassPathXmlApplicationContext("spring-config.xml");
+        AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext();
+        appContext.register(DateTimeBeanConfig.class, PropertiesPlaceholderBeanConfig.class, LoggingBeansConfig.class);
+        appContext.scan("com.mylab.spring.client", "com.mylab.spring.event", "com.mylab.spring.logging");
+        appContext.register(LoggingBeansConfig.class);
+        appContext.scan("com.mylab.spring");
+        appContext.refresh();
 
         App app = appContext.getBean("app", App.class);
 
