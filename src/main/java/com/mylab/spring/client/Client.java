0519e0988ebc11d466c9722df62adf26e0afafa0
Nick <nikolay.kovalev92@gmail.com>
2016-06-28 17:23:40 +0300

    Add default properties definition with SpEL

--- src/main/java/com/mylab/spring/client/Client.java
+++ src/main/java/com/mylab/spring/client/Client.java
@@ -14,7 +14,7 @@ public class Client {
     }
 
     @Autowired
-    public void setId(@Value("${client.id}")Long id) {
+    public void setId(@Value("${client.id:#{new java.util.Random().nextInt(T(java.lang.Integer).MAX_VALUE) + 1}}")Long id) {
         this.id = id;
     }
 
@@ -23,7 +23,9 @@ public class Client {
     }
 
     @Autowired
-    public void setFullName(@Value("${client.name}")String fullName) {
+    public void setFullName(@Value("${client.name:" +
+            "#{T(java.lang.System).getProperty('os.name').startsWith('Windows')" +
+            "? systemEnvironment['USERNAME'] : systemEnvironment['USER']}}")String fullName) {
         this.fullName = fullName;
     }
 
@@ -32,7 +34,7 @@ public class Client {
     }
 
     @Autowired
-    public void setGreeting(@Value("${client.greeting}")String greeting) {
+    public void setGreeting(@Value("${client.greeting:#{'Hello'}}")String greeting) {
         this.greeting = greeting;
     }
 }
