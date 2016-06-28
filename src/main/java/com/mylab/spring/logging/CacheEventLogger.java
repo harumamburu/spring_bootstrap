423819fe4c4053abbb1a21419883a680a7053cb5
Nick <nikolay.kovalev92@gmail.com>
2016-06-28 12:27:03 +0300

    Fix cached event logger cache not being initialized

--- src/main/java/com/mylab/spring/logging/CacheEventLogger.java
+++ src/main/java/com/mylab/spring/logging/CacheEventLogger.java
@@ -1,24 +1,19 @@
 package com.mylab.spring.logging;
 
 import com.mylab.spring.event.Event;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 
 import javax.annotation.PreDestroy;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CacheEventLogger extends FileEventLogger {
 
-    private int cacheSize;
-    private List<Event> cache;
+    private final int cacheSize;
+    private final List<Event> cache;
 
-    public int getCacheSize() {
-        return cacheSize;
-    }
-
-    @Autowired
-    public void setCacheSize(@Value("${logging.cache.size}")int cacheSize) {
+    public CacheEventLogger(int cacheSize) {
         this.cacheSize = cacheSize;
+        cache = new ArrayList<>(cacheSize);
     }
 
     @Override
