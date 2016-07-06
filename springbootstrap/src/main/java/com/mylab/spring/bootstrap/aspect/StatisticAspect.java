91d9a5e6ec1e1ecdc1494fb89e230ae4e3a1935d
Nick <nikolay.kovalev92@gmail.com>
2016-07-06 16:06:06 +0300

    Refactor event logging statistic aspect to utilize map of counters

--- springbootstrap/src/main/java/com/mylab/spring/bootstrap/aspect/StatisticAspect.java
+++ springbootstrap/src/main/java/com/mylab/spring/bootstrap/aspect/StatisticAspect.java
@@ -1,39 +1,35 @@
 package com.mylab.spring.bootstrap.aspect;
 
+import com.mylab.spring.bootstrap.logging.EventLogger;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Aspect
 public class StatisticAspect {
 
-    private final static AtomicInteger CONSOLE_WRITES_COUNTER = new AtomicInteger();
-    private final static AtomicInteger FILE_WRITES_COUNTER = new AtomicInteger();
-    private final static AtomicInteger CACHE_WRITES_COUNTER = new AtomicInteger();
-    private final static AtomicInteger COMBINED_WRITES_COUNTER = new AtomicInteger();
+    private final static Map<Class<? extends EventLogger>, AtomicInteger> COUNTERS = new ConcurrentHashMap<>(4, 0.9f, 1);
 
-    @Pointcut("execution(* *.logEvent(com.mylab.spring.bootstrap.event.Event))")
+    @Pointcut("execution(* com.mylab.spring.bootstrap.logging.*.logEvent(com.mylab.spring.bootstrap.event.Event))")
     private void logEventMethod() {};
 
-    @AfterReturning("logEventMethod() && within(com.mylab.spring.bootstrap.logging.ConsoleEventLogger)")
-    private void incrementConsoleWritings() {
-        CONSOLE_WRITES_COUNTER.incrementAndGet();
+    @AfterReturning("logEventMethod()")
+    private void incrementConsoleWritings(JoinPoint joinPoint) {
+        Class joinPointObjectType = joinPoint.getTarget().getClass();
+        if (EventLogger.class.isAssignableFrom(joinPointObjectType)) {
+            AtomicInteger counter = Optional.ofNullable(COUNTERS.get(joinPointObjectType)).orElse(new AtomicInteger());
+            counter.incrementAndGet();
+            COUNTERS.put(joinPointObjectType, counter);
+        }
     }
 
-    @AfterReturning("logEventMethod() && within(com.mylab.spring.bootstrap.logging.FileEventLogger)")
-    private void incrementFileWritings() {
-        FILE_WRITES_COUNTER.incrementAndGet();
-    }
-
-    @AfterReturning("logEventMethod() && within(com.mylab.spring.bootstrap.logging.CacheEventLogger)")
-    private void incrementCacheWriting() {
-        CACHE_WRITES_COUNTER.incrementAndGet();
-    }
-
-    @AfterReturning("logEventMethod() && within(com.mylab.spring.bootstrap.logging.CombinedEventLogger)")
-    private void incrementCombinedWriting() {
-        COMBINED_WRITES_COUNTER.incrementAndGet();
+    public Map<Class<? extends EventLogger>, AtomicInteger> getCounters() {
+        return COUNTERS;
     }
 }
