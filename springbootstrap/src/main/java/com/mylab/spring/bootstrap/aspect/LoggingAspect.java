e1f03651066473733c6d786d23de44cbfb6c2316
Nick <nikolay.kovalev92@gmail.com>
2016-07-06 15:21:24 +0300

    Added aspect failure logging

--- springbootstrap/src/main/java/com/mylab/spring/bootstrap/aspect/LoggingAspect.java
+++ springbootstrap/src/main/java/com/mylab/spring/bootstrap/aspect/LoggingAspect.java
@@ -6,18 +6,37 @@ import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Value;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Field;
+import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 @Aspect
 public class LoggingAspect {
 
-    @Resource(name = "logger")
     private Logger LOG;
+    @Value("${logging.error.pattern}")
+    private String handlerPattern;
+
+    @PostConstruct
+    private void init() {
+        LOG = Logger.getLogger(this.getClass().getSimpleName());
+        try {
+            FileHandler handler = new FileHandler(handlerPattern, true);
+            handler.setLevel(Level.WARNING);
+            handler.setFormatter(new SimpleFormatter());
+            LOG.addHandler(handler);
+        } catch (IOException exc) {
+            LOG.log(Level.WARNING, "FAILED TO ACCESS ERROR LOG FILE", exc);
+        }
+    }
 
     @Pointcut("execution(* com.mylab.spring.bootstrap.logging.*.logEvent(com.mylab.spring.bootstrap.event.Event))")
     private void allLogEventMethods() {};
@@ -49,6 +68,6 @@ public class LoggingAspect {
 
     @AfterThrowing(pointcut = "this(com.mylab.spring.bootstrap.logging.EventLogger)", throwing = "exc")
     private void logAfterLoggingException(RuntimeException exc) {
-        LOG.log(Level.WARNING, "!!! an error occurred while accessing log file location info !!!", exc);
+        LOG.log(Level.WARNING, "!!! an error occurred while accessing event log file location info !!!", exc);
     }
 }
