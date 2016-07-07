b3a65e256bad619373616c1bafa8019cd98b41a1
Nick <nikolay.kovalev92@gmail.com>
2016-07-07 12:46:45 +0300

    Add db logging logic

--- springbootstrap/src/main/java/com/mylab/spring/bootstrap/logging/DBLogger.java
+++ springbootstrap/src/main/java/com/mylab/spring/bootstrap/logging/DBLogger.java
@@ -4,22 +4,33 @@ import com.mylab.spring.bootstrap.event.Event;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.annotation.PostConstruct;
+import java.sql.SQLException;
 
 public class DBLogger implements EventLogger {
 
     private final JdbcTemplate template;
+    private final String createTableQuery;
+    private final String insertEventQuery;
 
-    public DBLogger(JdbcTemplate template) {
+    public DBLogger(JdbcTemplate template, String createTableQuery, String insertEventQuery) {
         this.template = template;
+        this.createTableQuery = createTableQuery;
+        this.insertEventQuery = insertEventQuery;
     }
 
     @Override
     public void logEvent(Event event) {
-
+        template.update(insertEventQuery, event.getId(), event.getCreationDate(),
+                event.getStringEvent(), event.getType().name());
     }
 
     @PostConstruct
-    private void createDbTables() {
-
+    private void createDbTables() throws SQLException{
+        // get CREATE TABLE <NAME> statement, split by spaces and get <NAME>
+        String tableName = createTableQuery.split("(?= ?\\()")[0].split("\\s")[2];
+        // check if table already exists
+        if (!template.getDataSource().getConnection().getMetaData().getTables(null, null, tableName, null).next()) {
+            template.execute(createTableQuery);
+        }
     }
 }
