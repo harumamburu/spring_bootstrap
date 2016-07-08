a9f9e09ee4d5fda8dddcf567f2f7221c75f136cc
Nick <nikolay.kovalev92@gmail.com>
2016-07-08 12:21:08 +0300

    Modify service tests structure to a more generalized one

--- springcoredemoapp/src/test/java/com/mylab/spring/coredemo/test/service/AbstractServiceTest.java
+++ springcoredemoapp/src/test/java/com/mylab/spring/coredemo/test/service/AbstractServiceTest.java
@@ -2,7 +2,9 @@ package com.mylab.spring.coredemo.test.service;
 
 import com.mylab.spring.coredemo.service.Service;
 import com.mylab.spring.coredemo.test.BaseTest;
+import org.testng.annotations.Test;
 
+@Test(groups = "serviceTest", dependsOnGroups = "daoTest")
 public abstract class AbstractServiceTest<T extends Service> extends BaseTest {
 
     protected T service;
