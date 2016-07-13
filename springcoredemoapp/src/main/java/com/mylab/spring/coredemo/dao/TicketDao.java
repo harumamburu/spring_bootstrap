95fb6bb9e4a8765a07a385e75b51358cedefd8d8
harumamburu <nikolay.kovalev92@gmail.com>
2016-07-13 14:28:26 +0300

    Remove unused imports from ticket dao interface

--- springcoredemoapp/src/main/java/com/mylab/spring/coredemo/dao/TicketDao.java
+++ springcoredemoapp/src/main/java/com/mylab/spring/coredemo/dao/TicketDao.java
@@ -2,7 +2,6 @@ package com.mylab.spring.coredemo.dao;
 
 import com.mylab.spring.coredemo.entity.Event;
 import com.mylab.spring.coredemo.entity.Ticket;
-import com.mylab.spring.coredemo.entity.User;
 
 import java.util.List;
 
