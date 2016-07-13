ce83e58308323842a55b3b69043038f6ab8b1b6a
harumamburu <nikolay.kovalev92@gmail.com>
2016-07-13 11:43:41 +0300

    Add auditorium service implementation stub

---
+++ springcoredemoapp/src/main/java/com/mylab/spring/coredemo/service/AuditoriumService.java
@@ -0,0 +1,21 @@
+package com.mylab.spring.coredemo.service;
+
+import com.mylab.spring.coredemo.entity.Auditorium;
+
+import java.util.List;
+
+public class AuditoriumService extends AbstractService {
+
+
+    public List<Auditorium> getAllAuditoriums() {
+        return null;
+    }
+
+    public Integer getSeatsNumber(Long id) {
+        return null;
+    }
+
+    public List<Integer> getVipSeats(Long id) {
+        return null;
+    }
+}
