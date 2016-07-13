a6bb77be66e29c55b4e703d1088bfe8a7e4d0a6a
harumamburu <nikolay.kovalev92@gmail.com>
2016-07-13 14:25:34 +0300

    Refactor booking memory dao

--- springcoredemoapp/src/main/java/com/mylab/spring/coredemo/dao/memory/MemoryBookingDao.java
+++ springcoredemoapp/src/main/java/com/mylab/spring/coredemo/dao/memory/MemoryBookingDao.java
@@ -8,6 +8,7 @@ import com.mylab.spring.coredemo.entity.Event;
 import com.mylab.spring.coredemo.entity.User;
 
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -32,26 +33,20 @@ public class MemoryBookingDao extends AbstractMemoryDao<Booking> implements Book
 
     @Override
     public List<Booking> getBookingsForUser(User user) throws DaoException {
-        if (user == null) {
-            throw new IllegalDaoRequestException("User can't be null");
-        }
-        return getFilteredValues(booking -> booking.getUser().equals(user));
+        checkParameters(user == null);
+        return getFilteredValues(forUser(user));
     }
 
     @Override
     public List<Booking> getBookingsForEvent(Event event) throws DaoException {
-        if (event == null) {
-            throw new IllegalDaoRequestException("Event can't be null");
-        }
-        return getFilteredValues(booking -> booking.getTicket().getEvent().equals(event));
+        checkParameters(event == null);
+        return getFilteredValues(forEvent(event));
     }
 
     @Override
     public List<Booking> getBookingsForEventUser(Event event, User user) throws DaoException {
-        if (event == null || user == null) {
-            throw new IllegalDaoRequestException("Parameters can't be null");
-        }
-        return getFilteredValues(booking -> booking.getTicket().getEvent().equals(event) && booking.getUser().equals(user));
+        checkParameters(event == null || user == null);
+        return getFilteredValues(forEvent(event).and(forUser(user)));
     }
 
     private List<Booking> getFilteredValues(Predicate<Booking> filter) {
@@ -61,10 +56,30 @@ public class MemoryBookingDao extends AbstractMemoryDao<Booking> implements Book
                 .collect(Collectors.toList());
     }
 
+
+
+    private Predicate<Booking> forEvent(Event event) {
+        return booking -> booking.getTicket().getEvent().equals(event);
+    }
+
+    private Predicate<Booking> atDate(Date date) {
+        return booking -> booking.getTicket().getEvent().getDate().equals(date);
+    }
+
+    private Predicate<Booking> forUser(User user) {
+        return booking -> booking.getUser().equals(user);
+    }
+
     @Override
     protected boolean isSavedAlready(Booking entity) {
         return BOOKINGS.values().parallelStream()
                 .anyMatch(booking -> booking.getUser().equals(entity.getUser()) &&
                 booking.getTicket().equals(entity.getTicket()));
+    }
+
+    private void checkParameters(boolean parameterCheck) throws IllegalDaoRequestException {
+        if (parameterCheck) {
+            throw new IllegalDaoRequestException("Parameters can't be null");
+        }
     }
 }
