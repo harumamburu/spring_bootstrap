f921d668b3631ab0958f24fe9001fbafce46cc70
harumamburu <nikolay.kovalev92@gmail.com>
2016-08-10 14:15:22 +0300

    Make AbstractAspect class actually abstract

--- springbootstrap/src/main/java/com/mylab/spring/bootstrap/aspect/AbstractAspect.java
+++ springbootstrap/src/main/java/com/mylab/spring/bootstrap/aspect/AbstractAspect.java
@@ -2,7 +2,7 @@ package com.mylab.spring.bootstrap.aspect;
 
 import org.aspectj.lang.annotation.Pointcut;
 
-public class AbstractAspect {
+public abstract class AbstractAspect {
 
     @Pointcut("execution(* com.mylab.spring.bootstrap.logging.*.logEvent(com.mylab.spring.bootstrap.event.Event))")
     protected void logEventMethods() {};
